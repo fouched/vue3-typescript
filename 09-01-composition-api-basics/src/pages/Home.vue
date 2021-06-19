ee0c2df11a03fd5f5f6f3e3947a125caf9f4ac83
Fouche <fouche@limehouse.co.za>
2021-06-19 21:55:26 +0200

    init

---
+++ 09-01-composition-api-basics/src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		Home
+		<p>My name is {{ person.name }} and my age is {{ person.age }}</p>
+		<button @click="handleClick">click me</button>
+		<button @click="person.age++">increase age</button>
+		<input type="text" v-model="person.name" />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Person from '../types/Person'
+
+export default defineComponent({
+	name: 'Home',
+	setup() {
+		const person = ref<Person>({ name: 'Fouche', age: 49 })
+
+		const handleClick = () => {
+			person.value.name = 'Luigi'
+			person.value.age = 30
+		}
+
+		return { person, handleClick }
+	}
+})
+</script>
