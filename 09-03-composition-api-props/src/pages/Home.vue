9a32027c1fdcf1e71fb1598295895a24c0600dde
Fouche <fouche@limehouse.co.za>
2021-06-20 10:59:13 +0200

    init

---
+++ 09-03-composition-api-props/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="home">
+		<h1>Home</h1>
+    
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, watch, watchEffect } from 'vue'
+
+export default defineComponent({
+	name: 'Home',
+	setup() {
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'Lorem ipsum', id: 1},
+      {title: 'Top 5 CSS tips', body: 'Lorem ipsum', id: 2}
+    ])
+
+    return { posts }
+	}
+})
+</script>
