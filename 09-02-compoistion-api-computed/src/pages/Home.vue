2cd6b74484b13118c7e3307ba80c583684094432
Fouche <fouche@limehouse.co.za>
2021-06-20 10:33:34 +0200

    Added watching samples

--- 09-02-compoistion-api-computed/src/pages/Home.vue
+++ 09-02-compoistion-api-computed/src/pages/Home.vue
@@ -3,12 +3,13 @@
 		<h1>Home</h1>
     <input type="text" v-model="search">
     <p>search term: {{ search }}</p>
+    <button @click="stopWatching">stop watching</button>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
+import { defineComponent, ref, computed, watch, watchEffect } from 'vue'
 
 export default defineComponent({
 	name: 'Home',
@@ -16,11 +17,24 @@ export default defineComponent({
 			const names = ref(['Fouche', 'Marien', 'Emily', 'Olivia'])
       const search = ref('')
 
+      const stopWatch = watch(search, () => {
+        console.log("watch function run")
+      })
+
+      const stopWatchEffect = watchEffect(() => {
+        console.log("watch effect function run", search.value)
+      })
+
+      const stopWatching = () => {
+        stopWatch()
+        stopWatchEffect()
+      }
+
       const matchingNames = computed(() => {
         return names.value.filter((name) => name.includes(search.value))
       })
 
-			return { names, search, matchingNames }
+			return { names, search, matchingNames, stopWatching }
 	}
 })
 </script>
